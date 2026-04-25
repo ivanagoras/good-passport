5d5122b5f392f58de49e75b2be6993deb94c04e5
ivanagoras <64276205+ivanagoras@users.noreply.github.com>
2026-04-25 10:00:48 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+importar App desde "./App"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+<React.StrictMode>
+<Aplicación />
+</React.StrictMode>
+);
